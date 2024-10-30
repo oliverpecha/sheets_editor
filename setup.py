253950cb8604ce80a300bcafd6aa50b9d9b5a50d
oliverpecha <oliverpecha@gmail.com>
2024-10-30 23:26:13 +0100

    Name Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="sheets_exporter",
+    name="sheets_editor",  # Changed from sheets_exporter
     version="0.1.0",
     packages=find_packages(),
     install_requires=[
@@ -11,8 +11,8 @@ setup(
     ],
     author="Your Name",
     author_email="your.email@example.com",
-    description="A package for exporting data to Google Sheets",
+    description="A package for editing and managing Google Sheets",
     long_description=open('README.md').read(),
     long_description_content_type="text/markdown",
-    url="https://github.com/yourusername/sheets_exporter",
+    url="https://github.com/oliverpecha/sheets_editor",
 )
