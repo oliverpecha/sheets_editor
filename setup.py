fe5a6a5eee68241c8cc382ffcc6a0d247659c1da
oliverpecha <oliverpecha@gmail.com>
2024-10-31 22:00:44 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,8 +7,7 @@ setup(
     install_requires=[
         'gspread',
         'google-auth',
-        'pyyaml'
-    ],
+    ], 
     author="Your Name",
     author_email="your.email@example.com",
     description="A package for editing and managing Google Sheets",
