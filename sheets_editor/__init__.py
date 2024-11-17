8c92873eb675e0d4f1801ff03207e4a0a3c5a26c
oliverpecha <oliverpecha@gmail.com>
2024-11-17 19:15:54 +0100

    SheetDeleter

--- sheets_editor/__init__.py
+++ sheets_editor/__init__.py
@@ -1,4 +1,6 @@
-from .exporter import SheetsExporter
 from .config import SheetConfig
+from .exporter import SheetsExporter
+from .formatter import SheetFormatter
+from .deleter import SheetDeleter  
 
 __version__ = '0.1.0'
