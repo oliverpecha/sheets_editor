9aa4bccd2e6480decc7a9be9999d3e0197946075
oliverpecha <oliverpecha@gmail.com>
2024-11-21 20:19:09 +0100

    fix typo __init__.py

--- sheets_editor/__init__.py
+++ sheets_editor/__init__.py
@@ -2,5 +2,5 @@ from .config import SheetConfig
 from .exporter import SheetsExporter
 from .formatter import SheetFormatter
 from .deleter import SheetDeleter  
-from .updated import SheetUpdater
+from .updater import SheetUpdater
 __version__ = '0.1.0'
