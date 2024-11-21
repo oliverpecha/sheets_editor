ec660212d91fc5d3bfbed5f093a3abfb417ab350
oliverpecha <oliverpecha@gmail.com>
2024-11-21 17:42:27 +0100

    Create update.py

---
+++ sheets_editor/update.py
@@ -0,0 +1,13 @@
+class SheetUpdate:
+    def __init__(self, worksheet):
+        """Initialize with the Google Sheets worksheet instance."""
+        self.worksheet = worksheet
+
+    def update_image_formulas(self, image_cells):
+        """
+        Updates the worksheet with image formulas in the specified cells.
+        :param image_cells: List of tuples containing (row, column, image_formula)
+        """
+        for row, col, image_formula in image_cells:
+            # Set the image formula in the appropriate cell
+            self.worksheet.update_cell(row, col, image_formula)
